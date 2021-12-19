236fd27bc3e128da6a4ac506e4016010367c37bf
erenyenigul <tc.eren.yenigul@gmail.com>
2021-12-20 00:13:46 +0300

    Add update() method to PhysicalObjectLabel

--- src/ui/PhysicalObjectLabel.java
+++ src/ui/PhysicalObjectLabel.java
@@ -1,6 +1,7 @@
 package ui;
 
 import domain.physicalobjects.PhysicalObject;
+import domain.physicalobjects.Vector;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,8 +14,15 @@ public class PhysicalObjectLabel extends JLabel{
         this.physicalObject = physicalObject;
         this.setBackground(Color.GREEN);
         this.setOpaque(true);
+
+        Vector location = physicalObject.getLocation();
+        this.setBounds((int) location.getX(),(int) location.getY(), (int)physicalObject.getWidth(), (int)physicalObject.getHeight());
     }
 
+    public void update(){
+        Vector location = physicalObject.getLocation();
+        this.setBounds((int) location.getX(),(int) location.getY(), (int)physicalObject.getWidth(), (int)physicalObject.getHeight());
+    }
 
     public PhysicalObject getPhysicalObject() {
         return physicalObject;
