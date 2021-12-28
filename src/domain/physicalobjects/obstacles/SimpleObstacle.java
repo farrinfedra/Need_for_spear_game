8edea4376e43a63e0cda0376618860ad5a40932e
ATAKAN KARA <akara18@ku.edu.tr>
2021-12-28 17:47:27 +0300

    Move assets folder and fix service is null error

--- src/domain/physicalobjects/obstacles/SimpleObstacle.java
+++ src/domain/physicalobjects/obstacles/SimpleObstacle.java
@@ -1,11 +1,8 @@
 package domain.physicalobjects.obstacles;
 
 import domain.physicalobjects.Vector;
-import domain.physicalobjects.behaviors.collision.CollisionBehavior;
 import domain.physicalobjects.behaviors.collision.ObstacleCollisionBehavior;
-import domain.physicalobjects.behaviors.movement.MovementBehavior;
 import domain.physicalobjects.behaviors.movement.StationaryMovementBehavior;
-import domain.services.GameBoardService;
 import domain.services.Service;
 
 import javax.swing.*;
