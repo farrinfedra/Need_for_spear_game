1a4426744dac579f2f90cfbae4f45d4de99c099c
ATAKAN KARA <akara18@ku.edu.tr>
2021-12-29 21:54:42 +0300

    Change explosive obstacle to circler shape

--- src/domain/physicalobjects/obstacles/ExplosiveObstacle.java
+++ src/domain/physicalobjects/obstacles/ExplosiveObstacle.java
@@ -3,6 +3,7 @@ package domain.physicalobjects.obstacles;
 import domain.physicalobjects.Vector;
 import domain.physicalobjects.behaviors.collision.ExplosiveObstacleCollisionBehavior;
 import domain.physicalobjects.behaviors.movement.StationaryMovementBehavior;
+import domain.physicalobjects.boundingbox.SphereBoundingBox;
 import domain.services.Service;
 
 import javax.swing.*;
@@ -11,9 +12,10 @@ import java.util.List;
 public class ExplosiveObstacle extends Obstacle{
 
     public ExplosiveObstacle(Vector location, List<Service> services) {
-        super(location, null,40, 40,
+        super(location, null,100, 100,
                 new StationaryMovementBehavior(),
-                new ExplosiveObstacleCollisionBehavior(), 1, services);
+                new ExplosiveObstacleCollisionBehavior(), 1, services,
+                new SphereBoundingBox(location.add(new Vector(50,50)),50));
 
         setImage(new ImageIcon(this.getClass().getResource("/ui/assets/ExplosiveObstacleImage.png")));
     }
